c757cb02ea67dbad9ea8ba6f86220b4041e01302
raelulu <raelulu23@gmail.com>
2023-03-30 17:08:09 +0900

    feat:메인수정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Weather from '../components/main/Weather.jsx';
 import styled, { keyframes } from 'styled-components';
-import NavBar from '../components/mypage/NavBar.jsx'
+import NavBar from '../components/mypage/NavBar.jsx';
 import { useDispatch, useSelector } from 'react-redux';
 import { BsArrowRight } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
@@ -11,22 +11,23 @@ const Hello = styled.h1`
   font-family: 'GongGothicMedium';
   color: white;
   @media (max-width: 912px) {
-  font-size: 5vw;
+    font-size: 5vw;
   }
 `;
 const Div = styled.div`
   position: fixed;
   width: 100%;
   left: 50%;
-  transform: translate(-50% , 0);
-  z-index: 100;background: linear-gradient(120deg, #5800FF, #FFC600);
-  `;
+  transform: translate(-50%, 0);
+  z-index: 100;
+  background: linear-gradient(120deg, #5800ff, #ffc600);
+`;
 
 const Maindiv = styled.div`
   position: absolute;
   right: 5%;
   margin: 150px 5%;
-  color:white;
+  color: white;
   width: 50%;
   max-width: 1980px;
   height: max-content;
@@ -41,7 +42,7 @@ const Maindiv = styled.div`
 
 const CrBox = styled.div`
   /* position: absolute; */
-  padding : 5%;
+  padding: 5%;
   background-color: rgb(255, 198, 0, 0.2);
   width: 100%;
   border-radius: 20px;
@@ -49,7 +50,7 @@ const CrBox = styled.div`
   }
   @media (max-width: 426px) {
   }
-  `;
+`;
 
 const WetherDiv = styled.div`
   position: fixed;
@@ -64,29 +65,29 @@ const WetherDiv = styled.div`
   }
   @media (max-width: 426px) {
   }
-  `;
-  const ChBox = styled.div`
-    /* position: absolute; */
-    padding : 5%;
-    background-color: rgb(255, 198, 0, 0.2);
-    width: 100%;
-    border-radius: 20px;
-    margin: 5% 0;
-    font-size: 5vh;
-    text-align: center;
-    @media (max-width: 912px) {
-    }
-    font-size: 3.5vh;
-    @media (max-width: 426px) {
-    }
-    `;
+`;
+const ChBox = styled.div`
+  /* position: absolute; */
+  padding: 5%;
+  background-color: rgb(255, 198, 0, 0.2);
+  width: 100%;
+  border-radius: 20px;
+  margin: 5% 0;
+  font-size: 5vh;
+  text-align: center;
+  @media (max-width: 912px) {
+  }
+  font-size: 3.5vh;
+  @media (max-width: 426px) {
+  }
+`;
 // -------------------------------------------------------------
 const ChallengeItems = styled.div`
   // 위치 지정
   margin: 1%;
   padding: 1%;
   display: inline-block;
-  width: 20%;
+  // width: 20%;
   height: 10vh;
   color: black;
   background-color: white;
@@ -123,7 +124,7 @@ const ChallengeItem = styled.div`
     color: gray;
   }
 `;
-    
+
 const un_mount_icon = keyframes`
 from {
   opacity: 0;
@@ -133,21 +134,21 @@ to {
 }
 `;
 const GoInfoIcon = styled.div`
-position: absolute;
-top:63vh;
-left:50%;
-transform: translate(-50%, 0);
-font-size: 1vw;
-font-family: initial;
-font-weight: 900;
-color: #FFC600;
-animation: ${un_mount_icon} 0.5s ease-in-out;
-@media (max-width: 769px) {
+  position: absolute;
+  top: 63vh;
+  left: 50%;
+  transform: translate(-50%, 0);
+  font-size: 1vw;
+  font-family: initial;
+  font-weight: 900;
+  color: #ffc600;
+  animation: ${un_mount_icon} 0.5s ease-in-out;
+  @media (max-width: 769px) {
     font-size: 1px;
   }
 `;
 
-export default function Main({arrowShow}) {
+export default function Main({ arrowShow }) {
   const [isHover, setIsHover] = useState(null);
   const MouseOver = (i) => {
     infoShow && arrowShow ? setIsHover(null) : setIsHover(i);
@@ -157,45 +158,63 @@ export default function Main({arrowShow}) {
 
   const infoShow = useSelector((state) => state.challenge.infoShow);
   const challenge = useSelector((state) => state.challenge.challenge);
-  return <>
-    <Div>
-      <NavBar />
-    </Div>
-    <WetherDiv>
-      <Weather />
-    </WetherDiv>
-    <Maindiv>
-      {/* <Div1> */}
-      <CrBox>
-      {info.id == undefined ? 
-      <Hello>안녕하세요. WetoUser님.</Hello>
-      :
-      <Hello>안녕하세요. {info.nickName}님.</Hello>
-      }
-      <p style={{color : "white"}}>Weto에 오신 것을 환영합니다.
-      <br />
-      CREW에서 활동을 시작해보아요.</p>
-        <Link as={Link} to="/crew" style={{ textDecoration: "none" , color:"black" }}>
-          <MainCrew />
-        </Link>
-      </CrBox>
-      {/* </Div1> */}
-      <ChBox>
-        OPEN Challenge
-        <br />
-        <br />
-        {challenge.map((v, i) => (
-          <React.Fragment key={i}><Link as={Link} to="/challenge" style={{ textDecoration: "none" , color:"black" }}>
-            {isHover === i && <GoInfoIcon>Challenge 참여할 시, 1,000원 기부됩니다.</ GoInfoIcon>}
-        <ChallengeItems
-          onMouseOver={() => MouseOver(i)}
-          onMouseOut={() => setIsHover(null)}
+  return (
+    <>
+      <Div>
+        <NavBar />
+      </Div>
+      <WetherDiv>
+        <Weather />
+      </WetherDiv>
+      <Maindiv>
+        {/* <Div1> */}
+        <CrBox>
+          {info.id == undefined ? (
+            <Hello>안녕하세요. WetoUser님.</Hello>
+          ) : (
+            <Hello>안녕하세요. {info.nickName}님.</Hello>
+          )}
+          <p style={{ color: 'white' }}>
+            Weto에 오신 것을 환영합니다.
+            <br />
+            CREW에서 활동을 시작해보아요.
+          </p>
+          <Link
+            as={Link}
+            to="/crew"
+            style={{ textDecoration: 'none', color: 'black' }}
           >
-          <ChallengeItem>{v.name}</ChallengeItem>
-        </ChallengeItems></Link>
-      </React.Fragment>
-    ))}
-      </ChBox>
-    </Maindiv>
-  </>;
+            <MainCrew />
+          </Link>
+        </CrBox>
+        {/* </Div1> */}
+        <ChBox>
+          OPEN Challenge
+          <br />
+          <br />
+          {challenge.map((v, i) => (
+            <React.Fragment key={i}>
+              <Link
+                as={Link}
+                to="/challenge"
+                style={{ textDecoration: 'none', color: 'black' }}
+              >
+                {isHover === i && (
+                  <GoInfoIcon>
+                    Challenge 참여할 시, 1,000원 기부됩니다.
+                  </GoInfoIcon>
+                )}
+                <ChallengeItems
+                  onMouseOver={() => MouseOver(i)}
+                  onMouseOut={() => setIsHover(null)}
+                >
+                  <ChallengeItem>{v.name}</ChallengeItem>
+                </ChallengeItems>
+              </Link>
+            </React.Fragment>
+          ))}
+        </ChBox>
+      </Maindiv>
+    </>
+  );
 }
